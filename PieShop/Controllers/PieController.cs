f88fee1b404e66b66e25332fa4a260db46ac5283
akhileshgowda <akhileshgowda7@gmail.com>
2023-06-16 15:02:45 -0400

    Added images and bootstrap styling

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using PieShop.Models;
+﻿using PieShop.Models;
 using PieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
-namespace PieShop.Controllers
+namespace BethanysPieShop.Controllers
 {
-	public class PieController : Controller
-	{
-		private readonly IPieRepository _pieRepository;
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
 
-
-        public PieController(IPieRepository pieRepository,ICategoryRepository categoryRepository)
-		{
-			_pieRepository = pieRepository;
-			_categoryRepository = categoryRepository;
-
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
         }
 
-		public IActionResult List()
-		{
-			PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cakes");
-			return View(pieListViewModel);
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
 
-		}
-	}
+            //return View(_pieRepository.AllPies);
+
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            return View(piesListViewModel);
+        }
+    }
 }
-
